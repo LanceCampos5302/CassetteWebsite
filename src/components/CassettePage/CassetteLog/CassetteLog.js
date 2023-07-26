33f3f8d338fcbd9ac67290b82529b7b47ce4c110
LanceCamops5302 <54639300+LanceCampos5302@users.noreply.github.com>
2023-07-26 03:11:09 -0400

    Creation of CassetteLog

---
+++ src/components/CassettePage/CassetteLog/CassetteLog.js
@@ -0,0 +1,20 @@
+import './CassetteLog.css';
+
+const CassetteLog = ({mainText, dateCreated,totalTime}) => {
+
+    return (
+        <div className='CassetteLogBackground'>
+            <div className='CassetteLogDelete'>
+                <div className='CassetteLogDeleteButton'></div>
+            </div>
+            <div className='CassetteLogTitle CassetteLogTextCenter'>{mainText}</div>
+            <div className='CassetteLogDateCreated CassetteLogTextCenter'>{dateCreated}</div>
+            <div className='CassetteLogTotalLength CassetteLogTextCenter'>{totalTime}</div>
+            <div className='CassetteLogPlayButton'>
+                <div className='CassetteLogPlayButtonPause'></div>
+            </div>
+        </div>
+        );
+    }
+
+export default CassetteLog;
--- src/components/CassettePage/CassettePage.js
+++ src/components/CassettePage/CassettePage.js
@@ -1,19 +1,47 @@
 import './CassettePage.css';
+import React, { useState } from 'react';
 import SideNavBar from '../SideNavBar/SideNavBar.js';
-//import CassetteSide from '../CassetteSide/CassetteSide.js';
+import CassetteFront from '../CassetteFront/CassetteFront';
+import CassetteLog from './CassetteLog/CassetteLog';
 
 const CassettePage = () => {
+
+    const [activeButton, setActiveButton] = useState('logs');
 
     return (
         <div className='Background'>
             <SideNavBar/>
-            <div className='CassettePageContainer'>
+            <div className='CassetteHomeContainer'>
                 <div className='TopBox'>
                     <div className='EjectButton'>
                         <div className='EjectTriangle'></div>
                     </div>
                 </div>
-                <div className='MidBox'>Mid Box</div>
+                <div className='MidBox'>
+                    <div className='FrontCassetteContainer'>
+                        <CassetteFront mainText='Yellowish' subText='7/13/2023' backgroundColor=' var(--cassette-background)' highlightColor='#98990a'/>
+                    </div>
+                    <div className='CassetteLogSelection'>
+                        <div className={`CassetteLogButton ${activeButton === 'logs' ? 'CassetteLogSelectedButton' : 'CassetteLogUnSelectedButton'}`}onClick={() => setActiveButton('logs')}>Logs</div>
+                        <div className={`CassetteLogButton ${activeButton === 'info' ? 'CassetteLogSelectedButton' : 'CassetteLogUnSelectedButton'}`}onClick={() => setActiveButton('info')}>Info</div>
+                    </div>
+                    <div className='CassetteLogContainer'>
+                        <CassetteLog mainText='Yellowish_01' dateCreated='05/03/2002 3:15am' totalTime='5:32'/>
+                        <CassetteLog mainText='Yellowish_02' dateCreated='05/04/2002 11:51am' totalTime='27:01'/>
+                        <CassetteLog mainText='Yellowish_03' dateCreated='05/05/2002 7:12pm' totalTime='1:51'/>
+                        <CassetteLog mainText='Yellowish_04' dateCreated='05/06/2002 9:15am' totalTime='15:32'/>
+                        <CassetteLog mainText='Yellowish_05' dateCreated='05/07/2002 2:21pm' totalTime='7:01'/>
+                        <CassetteLog mainText='Yellowish_06' dateCreated='05/08/2002 6:22pm' totalTime='1:51'/>
+                        <CassetteLog mainText='Yellowish_03' dateCreated='05/05/2002 7:12pm' totalTime='1:51'/>
+                        <CassetteLog mainText='Yellowish_04' dateCreated='05/06/2002 9:15am' totalTime='15:32'/>
+                        <CassetteLog mainText='Yellowish_05' dateCreated='05/07/2002 2:21pm' totalTime='7:01'/>
+                        <CassetteLog mainText='Yellowish_06' dateCreated='05/08/2002 6:22pm' totalTime='1:51'/>
+                        <CassetteLog mainText='Yellowish_03' dateCreated='05/05/2002 7:12pm' totalTime='1:51'/>
+                        <CassetteLog mainText='Yellowish_04' dateCreated='05/06/2002 9:15am' totalTime='15:32'/>
+                        <CassetteLog mainText='Yellowish_05' dateCreated='05/07/2002 2:21pm' totalTime='7:01'/>
+                        <CassetteLog mainText='Yellowish_06' dateCreated='05/08/2002 6:22pm' totalTime='1:51'/>
+                    </div>
+                </div>
                 <div className='BottomBox'>Bottom Box</div>
             </div>
         </div>
